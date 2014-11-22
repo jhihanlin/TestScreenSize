9ffd87580e9ef008b70ef52f085270760d0f1e89
jhihanlin <jhihanlin@gmail.com>
2014-11-22 22:33:33 +0800

    two ways for finding screenSize

--- src/com/myExample/testScreenSize/activity/MainActivity.java
+++ src/com/myExample/testScreenSize/activity/MainActivity.java
@@ -1,10 +1,13 @@
 package com.myExample.testScreenSize.activity;
 
 import com.myExample.testScreenSize.utils.ScreenSizeHelper;
+import com.myExample.testScreenSize.utils.ScreenSizeHelper.ScreenSize;
 import com.myexcample.testscreensize.R;
 
+import android.R.integer;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -14,11 +17,35 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		ScreenSizeHelper screenSizeHelper = new ScreenSizeHelper();
-		int height = screenSizeHelper.getScreenSize(this);
+		ScreenSize screenSize = ScreenSizeHelper.getScreenSize(this);
+		int height = screenSize.heightPixels;
+		Log.d("debug", "height:" + height);
+		int width = screenSize.widthPixels;
+		Log.d("debug", "width:" + width);
+
+		ScreenSize screenSize2 = ScreenSizeHelper.getScreenSize2(this);
+		int height2 = screenSize.heightPixels;
+		Log.d("debug", "height2:" + height2);
+		int width2 = screenSize.widthPixels;
+		Log.d("debug", "width2:" + width2);
+
 		TextView textView = (TextView) findViewById(R.id.textView1);
 
 		RelativeLayout.LayoutParams layoutParams = (android.widget.RelativeLayout.LayoutParams) textView.getLayoutParams();
 		layoutParams.topMargin = height / 2;
+
+	}
+
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+		super.onWindowFocusChanged(hasFocus);
+		updateSizeInfo();
+	}
+
+	private void updateSizeInfo() {
+		RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.RelativeLayout1);
+		int w = relativeLayout.getWidth();
+		int h = relativeLayout.getHeight();
+		Log.d("debug", "height" + h);
 	}
 }
--- src/com/myExample/testScreenSize/utils/ScreenSizeHelper.java
+++ src/com/myExample/testScreenSize/utils/ScreenSizeHelper.java
@@ -1,38 +1,39 @@
 package com.myExample.testScreenSize.utils;
 
+import android.R.integer;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.support.v4.view.ViewPager;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Display;
+import android.view.View;
 import android.view.WindowManager;
 
 @SuppressLint("NewApi")
 public class ScreenSizeHelper {
-	Context context;
 
-	public static int widthPixels;
-	public static int heightPixels;
-	private static int mActionBarSize;
-	private static int mStatusBarSize;
+	public static ScreenSize getScreenSize(Context context) {
 
-	public static int getScreenSize(Context context) {
 		WindowManager wm = (WindowManager) context.getSystemService(
 				Context.WINDOW_SERVICE);
 
 		Display display = wm.getDefaultDisplay();
 		DisplayMetrics metrics = new DisplayMetrics();
 		display.getMetrics(metrics);
-		widthPixels = metrics.widthPixels;
-		heightPixels = metrics.heightPixels;
-		Log.d("debug", "widthPixels" + widthPixels);
-		Log.d("debug", "heightPixels" + heightPixels);
+
+		int width = metrics.widthPixels;
+		Log.d("debug", "width" + width);
+
+		int height = metrics.heightPixels;
+		Log.d("debug", "height" + height);
 
 		// get action bar height
 		final TypedArray styledAttributes = context.getTheme().obtainStyledAttributes(
 				new int[] { android.R.attr.actionBarSize });
-		mActionBarSize = (int) styledAttributes.getDimension(0, 0);
+		int mActionBarSize = (int) styledAttributes.getDimension(0, 0);
 		styledAttributes.recycle();
 		Log.d("debug", "mActionBarSize" + mActionBarSize);
 
@@ -43,7 +44,32 @@ public class ScreenSizeHelper {
 			mStatusBarSize = context.getResources().getDimensionPixelSize(statusId);
 			Log.d("debug", "mStatusBarSize" + mStatusBarSize);
 		}
-		heightPixels = heightPixels - mActionBarSize - mStatusBarSize;
-		return heightPixels;
+
+		height = height - mActionBarSize - mStatusBarSize;
+
+		ScreenSize screenSize = new ScreenSize();
+		screenSize.heightPixels = height;
+		screenSize.widthPixels = width;
+		screenSize.mActionBarSize = mActionBarSize;
+		screenSize.mStatusBarSize = mStatusBarSize;
+		return screenSize;
+	}
+
+	public static class ScreenSize {
+		public int widthPixels;
+		public int heightPixels;
+		public int mActionBarSize;
+		public int mStatusBarSize;
+
+	}
+
+	public static ScreenSize getScreenSize2(Activity activity) {
+		View view = activity.getWindow().getDecorView().findViewById(android.R.id.content);
+		int height = view.getHeight();
+		int width = view.getWidth();
+		ScreenSize screenSize = new ScreenSize();
+		screenSize.heightPixels = height;
+		screenSize.widthPixels = width;
+		return screenSize;
 	}
 }
